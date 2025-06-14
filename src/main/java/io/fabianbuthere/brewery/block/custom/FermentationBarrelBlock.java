ca710ae2dc0c2f0d93cedcf60baf3bc02a5b5d61
Fabian2611 <fabian.schwarz2010@gmail.com>
2025-06-14 10:11:30 +0200

    Removed old TODO

---
+++ src/main/java/io/fabianbuthere/brewery/block/custom/FermentationBarrelBlock.java
@@ -0,0 +1,42 @@
+package io.fabianbuthere.brewery.block.custom;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.Nullable;
+
+public class FermentationBarrelBlock extends BaseEntityBlock {
+    private static final VoxelShape SHAPE = Block.box(0, 0, 0, 16, 16, 16);
+
+    public FermentationBarrelBlock(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pMovedByPiston) {
+        super.onRemove(pState, pLevel, pPos, pNewState, pMovedByPiston);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return SHAPE;
+    }
+
+    @Override
+    public RenderShape getRenderShape(BlockState pState) {
+        return RenderShape.MODEL;
+    }
+
+    @Override
+    public @Nullable BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
+        return null;
+    }
+}
